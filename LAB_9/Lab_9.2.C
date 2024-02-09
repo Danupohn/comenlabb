1d76484feb359c5d25281c4642602f001655c4f8
Kuang <Danuporn.saekhoo@gmail.com>
2024-02-10 00:04:10 +0700

    แลป 9

---
+++ LAB_9/Lab_9.2.C
@@ -0,0 +1,58 @@
+[วันพฤหัสบดี 15:45] สนธยา ทาจำปา
+#include <LiquidCrystal_I2C.h>
+#include <EEPROM.h>
+#define LCD_addr 0x27
+LiquidCrystal_I2C lcd(LCD_addr, 16, 2);
+unsigned int i = 0;
+int time = 0;
+void setup() {
+  lcd.begin();
+  Serial.begin(9600);
+  i = EEPROM.read(5);
+  time = EEPROM.read(6);
+  ++time;
+ 
+}
+void loop() {
+  lcd.clear();
+  lcd.setCursor(7, 0);
+  if (i < 10)
+    lcd.print('0');
+  EEPROM.write(5,i);
+  lcd.print(i);
+  Serial.println(i);
+  lcd.setCursor(7, 1);
+  EEPROM.write(6,time);
+  lcd.print(time);
+  i++;
+  delay(800);
+}
+[วันพฤหัสบดี 15:53] สนธยา ทาจำปา
+#include <LiquidCrystal_I2C.h>
+#include <EEPROM.h>
+#define LCD_addr 0x27
+LiquidCrystal_I2C lcd(LCD_addr, 16, 2);
+unsigned int i = 0;
+int time = 0;
+void setup() {
+  lcd.begin();
+  Serial.begin(9600);
+  i = EEPROM.read(5);
+  time = EEPROM.read(6);
+  ++time;
+ 
+}
+void loop() {
+  lcd.clear();
+  lcd.setCursor(7, 0);
+  if (i < 10)
+    lcd.print('0');
+  EEPROM.write(5,i);
+  lcd.print(i);
+  Serial.println(i);
+  lcd.setCursor(7, 1);
+  EEPROM.write(6,time);
+  lcd.print(time);
+  i++;
+  delay(800);
+}
