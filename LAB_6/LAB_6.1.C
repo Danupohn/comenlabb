3da74b18ddaa7fdfa3809dc309bdfd035c87e2eb
Kuang <Danuporn.saekhoo@gmail.com>
2024-02-10 02:02:49 +0700

    LAB 6

--- LAB_6/LAB_6.1.C
+++ LAB_6/LAB_6.1.C
@@ -1,44 +1,42 @@
-#define LOOP_PLAY 300
-const char pin_seg[] = { 2,3, 4, 5, 6, 7, 8, 9, 10, 11, 12,};// digit 1, 2, 3, a, b, c, d, e, f, g, dp
-const char led_num[] = {0x5C,0x54,0x00,0x7C,0X79,0x6E,0x1C};
+#define LOOP_PLAY 150
+const char pin_seg[] = {2,3,4,5,6,7,8,9,10,11,12,}; // digit 1, 2, 3, a, b, c, d, e, f, g, dp
+const char led_num[] = {   0x3F,   0x06,   0x5B,   0x4F, 0x66,0x6D,0x7D, 0x07, 0x7F,  0x6F,  0x77,  0x7C,  0x39, 0x5E,  0x79,  0x71,   0x80,};
 int iNum = 0;
 int iPin = 0;
-void setup() {
-  iPin = sizeof(pin_seg);
-  for (int i = 0; i < iPin; i++) {
-    pinMode(pin_seg[i], OUTPUT);
-  }
+void setup()
+{
+    iPin = sizeof(pin_seg);
+    for (int i = 0; i < iPin; i++)
+    {
+        pinMode(pin_seg[i], OUTPUT);
+    }
 }
-void loop() {
-  for (int i = 0; i < LOOP_PLAY; i++) {
-    DisplayNum(pin_seg[2], 1);
-    DisplayNum(pin_seg[1], 0);
-    DisplayNum(pin_seg[0], 2);
-  }
-  for (int i = 0; i < LOOP_PLAY; i++) {
-    DisplayNum(pin_seg[0], 3);
-    DisplayNum(pin_seg[2], 4);
-    DisplayNum(pin_seg[1], 5);
-  }
-   for (int i = 0; i < LOOP_PLAY; i++) {
-    DisplayNum(pin_seg[0], 5);
-    DisplayNum(pin_seg[2], 6);
-    DisplayNum(pin_seg[1], 0);
-  }
-  if(iNum++ > 999) iNum = 0;
+void loop()
+{
+    for (int i = 0; i < LOOP_PLAY; i++)
+    {
+        DisplayNUM(pin_seg[0], ((iNum % 100) % 10));
+        DisplayNUM(pin_seg[0], ((iNum % 100) / 10));
+        DisplayNUM(pin_seg[0], (iNum / 100));
+    }
+    if(iNum++ > 999) iNum = 0;
 }
-void DisplayNum(int digit, int num) {
-  char digital = led_num[num];
-  if (digit != pin_seg[2]) digitalWrite(pin_seg[2], HIGH);
-  if (digit != pin_seg[1]) digitalWrite(pin_seg[1], HIGH);
-  if (digit != pin_seg[0]) digitalWrite(pin_seg[0], HIGH);
-  digitalWrite(digit, LOW);
-  for (int i = 3; i < iPin; i++) {
-    if (digital & 0x01)
-      digitalWrite(pin_seg[i], HIGH);
-    else
-      digitalWrite(pin_seg[i], LOW);
-    digital = digital >> 1;
-  }
-   delay(1);
-}
+void DisplayNum(int digit, int num)
+{
+    char digital = led_num[num];
+    if (digit != pin_seg[0])
+        digitalWrite(pin_seg[0], HIGH);
+    if (digit != pin_seg[1])
+        digitalWrite(pin_seg[1], HIGH);
+    if (digit != pin_seg[2])
+        digitalWrite(pin_seg[2], HIGH);
+    digitalWrite(digit, LOW);
+    for (int i = 3; i < iPin; i++)
+    {
+        if (digital & 0x01)
+            digitalWrite(pin_seg[i], HIGH);
+        else
+            digitalWrite(pin_seg[i], LOW);
+
+        digital = digital >> 1;
+    }
